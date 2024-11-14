6bf7e2e51fb241221794b31631d62e31d11db3b3
CristianMarNav <cristianmartineznavarro4@gmail.com>
2024-11-14 12:40:32 +0100

    "Inicio de la estructura básica del juego B00MERMAN"

---
+++ src/MartinezCristianEjercicio1.java
@@ -0,0 +1,16 @@
+public class MartinezCristianEjercicio1{
+    public static void main(String[]args){
+
+        //Impresion de mensaje de bienvenida al usuario.
+        System.out.println("¡Bienvenido a B00MBERMAN!");
+
+
+
+
+
+
+
+
+
+    }
+}
