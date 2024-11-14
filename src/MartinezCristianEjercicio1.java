d22487919a5d226e39cf17e9c2541ab71d9bff2e
CristianMarNav <cristianmartineznavarro4@gmail.com>
2024-11-14 18:51:42 +0100

    "Solicitud de tamaño del campo de batalla y colocación de bombas aleatorias"

--- src/MartinezCristianEjercicio1.java
+++ src/MartinezCristianEjercicio1.java
@@ -1,19 +1,20 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class MartinezCristianEjercicio1{
-    // "Metodo main"(metodo principal donde empieza el juego)
-    public static void main(String[]args){
+public class MartinezCristianEjercicio1 {  // "Metodo main"(metodo principal donde empieza el juego)
+    public static void main(String[] args) {
 
         /*Impresion de mensaje de bienvenida al usuario.
-        Este mensaje aparecerá al inicio para que el usuario sepa que está en el juego Bomberman.
-         */
+        Este mensaje aparecerá al inicio para que el usuario sepa que está en el juego Bomberman.*/
+
         System.out.println("¡Bienvenido a B00MBERMAN!");
 
-        // Importación de la clase Scanner para leer entrada de usuario.
+        // Importamos la clase "SCANNER" para leer entrada de usuario.
 
         Scanner input = new Scanner(System.in);
 
-        //Usuario introduce el valor de filas y Scanner guarda su informacióN
+        //Usuario introduce el valor de filas y Scanner guarda su información.
+
         System.out.println("Introduce el número de filas del campo de batalla");
         int filas = input.nextInt();
         input.nextLine(); // Limpio el buffer después de leer un número.
@@ -22,30 +23,66 @@ public class MartinezCristianEjercicio1{
         int columnas = input.nextInt();
         input.nextLine();//Limpio el buffer después de leer un número.
 
-        // Imprimo al usuario el número de filas y columnas que ha introducido.
-        // Aquí seguirán los próximos pasos del juego.
-        System.out.println("Terreno de batalla: "+filas+" filas y "+columnas+" columnas");
-        System.out.println("Atención: Riesgo de bombas en el area y tenga precaución, buena suerte");
+        // Imprimo y muestro al usuario el número de filas y columnas que ha introducido.
+
+        System.out.println("Terreno de batalla: " + filas + " filas y " + columnas + " columnas");
+        System.out.println("¡Atención: Riesgo de bombas en el área!");
+        System.out.println("¡Y recuerde!: Tenga mucha precaución y buena suerte...");
 
         //Cierro la herramienta Scanner
+
         input.close();
 
+        // Ahora creamos una matriz para representar el terreno de juego y batalla (usamos char[][]).
 
+        char[][] terreno = new char[filas][columnas];
 
+        // Con "FOR" podremos inicializar cada celda del terreno con espacios vacios.
 
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                terreno[i][j] = ' '; // Uso (' ') para representar un espacio vacio.
+            }
+        }
 
+        // Importamos la clase "RANDOM" para la colocación de las bombas aleatoriamente.
 
+        Random rand = new Random();
+        int numBombas = 6; // Numero de bombas a colocar (valor que se puede cambiar).
 
+        for (int i = 0; i < numBombas; i++) {
+            int fila = rand.nextInt(filas); // Generamos una fila aleatoria.
+            int columna = rand.nextInt(columnas); // Generamos una columna aleatoria.
+            terreno[fila][columna] = 'X'; // "X" representa la bomba.
+        }
 
+        // Ahora vamos a imprimir y mostrar al usuario el terreno con las bombas colocadas
+        System.out.println("¡ULTIMO AVISO!¡PELIGRO!¡SE B00MBAS EN EL CMAPO DE BATALLA");
 
-
-
-
-
-
-
-
-
-
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(terreno[i][j] + " ");// Imprime cada celda
+            }
+            System.out.println(); // Salto de linea al final de cada fila
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
