9ec2e2140bf185d6add40b3ade64d2d1a80dfc2d
CristianMarNav <cristianmartineznavarro4@gmail.com>
2024-11-15 02:13:58 +0100

    Implementado ranking de explosiones, corregidos cálculos de explosión y mejoras en la visualización de la matriz. Finalización del ejercicio de Bomberman.

--- src/MartinezCristianEjercicio1.java
+++ src/MartinezCristianEjercicio1.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -29,27 +30,28 @@ public class MartinezCristianEjercicio1 {  // "Metodo main"(metodo principal don
         System.out.println("¡Atención: Riesgo de bombas en el área!");
         System.out.println("¡Y recuerde!: Tenga mucha precaución y buena suerte...");
 
-        // Ahora creamos una matriz para representar el terreno de juego y batalla (usamos char[][]).
+        // Ahora creamos una matriz para representar el terreno de juego y batalla (usamos int[][]).
 
-        char[][] terreno = new char[filas][columnas];
+        int[][] terreno = new int[filas][columnas];
 
-        // Con "FOR" podremos inicializar cada celda del terreno con espacios vacios.
-
+        // Inicializamos con la clase "RANDOM" la matriz con valores aleatorios entre 1 y 9.
+        Random rand = new Random();
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
-                terreno[i][j] = ' '; // Uso (' ') para representar un espacio vacio.
+                terreno[i][j] = rand.nextInt(9)+1; // Número aleatoria que sera entre 1 y 9.
             }
         }
 
-        // Importamos la clase "RANDOM" para la colocación de las bombas aleatoriamente.
+        // Lista Array para almacenar los valores de las explosiones.
+        ArrayList<Integer> rankingExplosiones = new ArrayList<>();
 
-        Random rand = new Random();
-        int numBombas = 6; // Numero de bombas a colocar (valor que se puede cambiar).
+        // Colocamos bombas aleatoria en el terreno.
 
+        int numBombas = 6;
         for (int i = 0; i < numBombas; i++) {
             int fila = rand.nextInt(filas); // Generamos una fila aleatoria.
             int columna = rand.nextInt(columnas); // Generamos una columna aleatoria.
-            terreno[fila][columna] = 'X'; // "X" representa la bomba.
+            terreno[fila][columna] = 0; // "0" representa la bomba.
         }
 
         // Solicitamos la posición inicial al jugador que sera 'P'.
@@ -62,46 +64,58 @@ public class MartinezCristianEjercicio1 {  // "Metodo main"(metodo principal don
         int columnaJugador = input.nextInt();
         input.nextLine(); // Limpio el buffer después de leer un número.
 
-        terreno[filaJugador][columnaJugador] = 'P'; /* Coloca al jugador en las coordenadas especificadas por el usuario
-                                                       en el campo de batalla, representando su posicion con la letra 'P'
-                                                       en la matriz */
+        // Mostramos el terreno inicial con las bombas y el jugador.
 
-        // Ahora vamos a imprimir y mostrar al usuario el terreno con las bombas colocadas
+        terreno[filaJugador][columnaJugador] = -1; // El jugador quedara representado por "-1".
+
+        // Ahora mostramos el terreno inicial.
 
         System.out.println("¡ULTIMO AVISO!¡PELIGRO!¡B00MBAS EN EL CAMPO DE BATALLA");
 
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
-                System.out.print(terreno[i][j] + " ");// Imprime cada celda
+                if (terreno[i][j] == -1) {
+                    System.out.print("P "); // Mostrar el jugador como 'P'
+                } else if (terreno[i][j] == 0) {
+                    System.out.print("X "); // Mostrar la bomba como 'X'.
+                } else {
+                    System.out.print(terreno[i][j] + " "); // Mostrar valores del terreno de juego.
+                }
             }
-            System.out.println(); // Salto de linea
+            System.out.println(); // Salto de línea para la siguiente fila.
         }
-          // Menú de opciones para el jugador
+          // Menú de opciones para el jugador.
         boolean continuar = true;
 
         while (continuar) {
 
-            mostrarMenu(); // Llamada al metodo mostrar menú
+            mostrarMenu(); // Llamada al metodo mostrar menú.
             int opcion = input.nextInt();
             input.nextLine(); // Limpio el buffer después de leer un número.
 
             switch (opcion){
                 case 0:
                     System.out.println("¡Hasta pornto!¡GAME 0VER!");
-                    continuar = false; // Termina el juego
+                    continuar = false; // Termina el juego.
                     break;
 
-                case 1: // Mostrar matriz
+                case 1: // Mostrar matriz.
                     System.out.println("Terreno del campo de batalla");
                     for (int i = 0; i < filas; i++) {
                         for (int j = 0; j < columnas; j++) {
-                            System.out.print(terreno[i][j] + " "); // Imprime cada celda.
+                            if (terreno[i][j] == -1) {
+                                System.out.print("P "); // Mostrar jugador como 'P'.
+                            } else if (terreno[i][j] == 0) {
+                                System.out.print("X "); // Mostrar bomba como 'X'.
+                            } else {
+                                System.out.print(terreno[i][j] + " "); // Mostrar valores del terreno de juego.
+                            }
                         }
-                        System.out.println(); // Salto de linea.
+                        System.out.println(); // Salto de línea para la siguiente fila.
                     }
                     break;
 
-                case 2: // Colocar Bomba
+                case 2: // Colocar Bomba.
                     System.out.println("Introduce las coordenadas (fila/columna) para colocar la bomba");
 
                     int filaBomba = input.nextInt();
@@ -110,19 +124,37 @@ public class MartinezCristianEjercicio1 {  // "Metodo main"(metodo principal don
                     input.nextLine(); // Limpio el buffer después de leer un número.
 
                     if (filaBomba >= 0 && filaBomba < filas && columnaBomba >= 0 && columnaBomba < columnas) {
-                        // Calcular la explosión (suma de la fija y columna
+                        // Calcular la explosión (suma de la fija y columna.
                         int explosion = 0;
 
-                        for (int i=0; i < columnas; i++) {
-                            explosion = explosion + (terreno[filaBomba][i] - '0'); // Sumar valores en la fila.
-                        } for (int i = 0; i < filas; i++) {
-                            explosion = explosion + (terreno[i][columnaBomba] - '0'); // Sumar valores en la columna.
+                        // Sumar los valores en la fila.
+                        for (int i = 0; i < columnas; i++) {
+                            if (terreno[filaBomba][i] != 0) { // No contar la bomba.
+                                explosion += terreno[filaBomba][i];
+                            }
                         }
-                        System.out.println("Valor explosión "+explosion);
-                        // Colocar la bomba
+                        // Sumar los valores en la columna.
+                        for (int i = 0; i < filas; i++) {
+                            if (terreno[i][columnaBomba] != 0) { // No contar la bomba.
+                                explosion += terreno[i][columnaBomba];
+                            }
+                        }
+
+                        System.out.println("Valor explosión " + explosion);
+                        // Colocar la bomba.
                         terreno[filaBomba][columnaBomba] = '0'; // Cambiar a 0 si la bomba ha explotado.
+                        // Guardamos el valor de la explosión en el ranking.
+                        rankingExplosiones.add(explosion);
+
                     } else {
                         System.out.println("Coordenadas no válidas, fuera de rango");
+                    }
+                    break;
+
+                case 3: // Ranking.
+                    System.out.println("Ranking de explosiones");
+                    for (int i = 0; i < rankingExplosiones.size(); i++) {
+                        System.out.println("Explosión "+(i+1)+": "+rankingExplosiones.get(i));
                     }
                     break;
 
@@ -139,7 +171,7 @@ public class MartinezCristianEjercicio1 {  // "Metodo main"(metodo principal don
         System.out.println("[0] Salir");
         System.out.println("[1] Mostrar matriz");
         System.out.println("[2] Poner bomba");
-
+        System.out.println("[3] Ranking de explosiones");
     }
 }
 
